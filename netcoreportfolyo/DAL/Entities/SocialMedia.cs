ec0fbd0c81aebf25e776b30a10cf2e4fd4a11750
Anıl Engin Keretli <116742352+anlkeretlii@users.noreply.github.com>
2025-05-24 21:43:53 +0300

    ilk ders

---
+++ netcoreportfolyo/DAL/Entities/SocialMedia.cs
@@ -0,0 +1,10 @@
+﻿namespace netcoreportfolyo.DAL.Entities
+{
+    public class SocialMedia
+    {
+        public int SocialMediaId { get; set; }
+        public string Title { get; set; }
+        public string URL { get; set; }
+        public string Icon { get; set; }
+    }
+}
---
+++ netcoreportfolyo/ViewComponents/_NavbarComponentPartial.cs.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+
+namespace netcoreportfolyo.ViewComponents
+{
+    public class _NavbarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
